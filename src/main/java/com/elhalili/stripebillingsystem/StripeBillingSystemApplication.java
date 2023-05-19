fb2fbcb883386ccfd16a413be76c7053695dc718
elhalili <aminhalili2002@gmail.com>
2023-05-19 23:57:10 +0100

    Initial commit

---
+++ src/main/java/com/elhalili/stripebillingsystem/StripeBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.elhalili.stripebillingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeBillingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StripeBillingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elhalili/stripebillingsystem/StripeBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elhalili.stripebillingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeBillingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
